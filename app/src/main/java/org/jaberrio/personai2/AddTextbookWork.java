9663818cf0b78f7b840cd9a563c2f3680c893abf
jaberrio <jaberrio88@hotmail.com>
2015-06-18 12:29:56 -0400

    First Commit Start 6/18/2015 Fragment Templates

---
+++ app/src/main/java/org/jaberrio/personai2/AddTextbookWork.java
@@ -0,0 +1,17 @@
+package org.jaberrio.personai2;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class AddTextbookWork extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_add_textbook_work, container, false);
+
+    }
+}
