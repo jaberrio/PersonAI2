10dc0914b7bd7542d85200d526ebf57ca10ea544
Jaberrio <jaberrio88@hotmail.com>
2015-06-19 10:51:41 -0400

    Calandar And List Added

---
+++ app/src/main/java/org/jaberrio/personai2/ListViewLoader.java
@@ -0,0 +1,14 @@
+package org.jaberrio.personai2;
+
+import android.app.ListActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+
+public class ListViewLoader extends ListActivity {
+    private ListView mainListView ;
+    private ArrayAdapter<String> listAdapter ;
+
+
+
+}
--- app/src/main/java/org/jaberrio/personai2/OverviewLand.java
+++ app/src/main/java/org/jaberrio/personai2/OverviewLand.java
@@ -6,14 +6,22 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.CalendarView;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class OverviewLand extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.fragment_overview_land, container, false);
+
+        ListView mainListView ;
+        ArrayAdapter<String> listAdapter ;
 
         CalendarView cV = (CalendarView)view.findViewById(R.id.calendarViewItem);
         final TextView date = (TextView)view.findViewById(R.id.date);
@@ -24,6 +32,26 @@ public class OverviewLand extends Fragment {
                 date.setText(String.valueOf(year) + ":" + String.valueOf(month) + ":" + String.valueOf(dayOfMonth));
             }
         });
+
+        mainListView = (ListView)view.findViewById(R.id.listTo);
+
+        String[] planets = new String[] { "Mercury", "Venus", "Earth", "Mars",
+                "Jupiter", "Saturn", "Uranus", "Neptune"};
+        ArrayList<String> planetList = new ArrayList<String>();
+        planetList.addAll( Arrays.asList(planets) );
+
+        // Create ArrayAdapter using the planet list.
+        listAdapter = new ArrayAdapter<String>(getActivity(), R.layout.list_today, planetList);
+
+        // Add more planets.
+        listAdapter.add( "Ceres" );
+        listAdapter.add( "Pluto" );
+        listAdapter.add( "Haumea" );
+        listAdapter.add( "Makemake" );
+        listAdapter.add( "Eris" );
+
+        // Set the ArrayAdapter as the ListView's adapter.
+        mainListView.setAdapter( listAdapter );
 
         return view;
     }
