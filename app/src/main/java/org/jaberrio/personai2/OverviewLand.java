fde955903fc35235b6c0cdbc71ebbaf2ef880511
Jaberrio <jaberrio88@hotmail.com>
2015-06-19 15:49:10 -0400

    Added Buttons That DO NOTHING FIXED

--- app/src/main/java/org/jaberrio/personai2/OverviewLand.java
+++ app/src/main/java/org/jaberrio/personai2/OverviewLand.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -14,11 +15,21 @@ import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class OverviewLand extends Fragment {
+public class OverviewLand extends Fragment implements View.OnClickListener {
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.fragment_overview_land, container, false);
+
+        final Button ament0 = (Button)view.findViewById(R.id.ament0);
+        final Button ament1 = (Button)view.findViewById(R.id.ament1);
+        final Button ament2 = (Button)view.findViewById(R.id.ament2);
+        final Button ament3 = (Button)view.findViewById(R.id.ament3);
+        final Button ament4 = (Button)view.findViewById(R.id.ament4);
+        final Button ament5 = (Button)view.findViewById(R.id.ament5);
+        final Button ament6 = (Button)view.findViewById(R.id.ament6);
+        final Button ament7 = (Button)view.findViewById(R.id.ament7);
 
         ListView mainListView ;
         ArrayAdapter<String> listAdapter ;
@@ -26,10 +37,19 @@ public class OverviewLand extends Fragment {
         CalendarView cV = (CalendarView)view.findViewById(R.id.calendarViewItem);
         final TextView date = (TextView)view.findViewById(R.id.date);
 
+        ament0.setOnClickListener(this);
+        ament1.setOnClickListener(this);
+        ament2.setOnClickListener(this);
+        ament3.setOnClickListener(this);
+        ament4.setOnClickListener(this);
+        ament5.setOnClickListener(this);
+        ament6.setOnClickListener(this);
+        ament7.setOnClickListener(this);
+//
         cV.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
-                date.setText(String.valueOf(year) + ":" + String.valueOf(month) + ":" + String.valueOf(dayOfMonth));
+                date.setText(String.valueOf(year) + "/" + String.valueOf(month + 1) + "/" + String.valueOf(dayOfMonth));
             }
         });
 
@@ -44,18 +64,21 @@ public class OverviewLand extends Fragment {
         listAdapter = new ArrayAdapter<String>(getActivity(), R.layout.list_today, planetList);
 
         // Add more planets.
-        listAdapter.add( "Ceres" );
+        listAdapter.add( "This is a really long text field to prove the concept of of multi line things" );
         listAdapter.add( "Pluto" );
-        listAdapter.add( "Haumea" );
-        listAdapter.add( "Makemake" );
+        listAdapter.add("Haumea" );
+        listAdapter.add("Makemake" );
         listAdapter.add( "Eris" );
 
         // Set the ArrayAdapter as the ListView's adapter.
-        mainListView.setAdapter( listAdapter );
+        mainListView.setAdapter(listAdapter);
 
         return view;
     }
 
 
+    @Override
+    public void onClick(View v) {
 
+    }
 }
